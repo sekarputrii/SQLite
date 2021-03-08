97fbdf6e981a8b36ed5974db8f5853b0fc010f52
sekarputrii <sekarputriretno83@gmail.com>
2021-03-08 15:38:58 +0700

    Activity

--- Activity1/app/src/main/java/com/example/activity1/MainActivity.java
+++ Activity1/app/src/main/java/com/example/activity1/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.activity1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextTextPersonName , editTextTextPassword;
+    Button button;
+    String Username, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Username = editTextTextPersonName.getText().toString();
+                Password = editTextTextPassword.getText().toString();
+
+                //Untuk menampilkan toast
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "Email : "+ Username +"Password : "+ Password +"", Toast.LENGTH_LONG);
+                t.show();
+            }
+        });
+    }
+    public boolean validasiData(){
+        String nameActive = "sekarputri@gmail.com";
+        String passwordActive = "Sekar83";
+
+        if(nameActive.equals(editTextTextPersonName.getText().toString()) && passwordActive.equals(editTextTextPassword.getText().toString())){
+            return true;
+        }
+        return false;
     }
 }
